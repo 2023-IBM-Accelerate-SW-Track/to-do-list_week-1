2b3b5cea3deab087652b111d6095d3fc2e951032
Rotimi Awani <rotimi.awani@ibm.com>
2022-05-10 14:35:15 -0700

    creating boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from "./component/header"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavbarComp from './component/navigation/NavbarComp';
 
 function App() {
   return (
     <div className="App">
-    <Header/>
+      <NavbarComp/>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+  
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>TODO list app home page</p> 
+      </div>
+    )
+  }
+}
