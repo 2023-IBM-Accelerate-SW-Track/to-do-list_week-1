0270ca6c8ce79279cb47b79f496370f70d28875d
Sujeily Fonseca <sujeily.fonseca@ibm.com>
2023-06-02 18:30:44 -0400

    feat: .gitignore and package dependencies (#7)
    * feat: .gitignore
    
    Signed-off-by: Sujeily Fonseca <sujeily.fonseca@ibm.com>
    
    * refactor: Removed unused component reference
    
    Signed-off-by: Sujeily Fonseca <sujeily.fonseca@ibm.com>
    
    * feat: Updated package dependencies
    
    Signed-off-by: Sujeily Fonseca <sujeily.fonseca@ibm.com>
    
    ---------
    
    Signed-off-by: Sujeily Fonseca <sujeily.fonseca@ibm.com>

--- src/component/navigation/NavbarComp.js
+++ src/component/navigation/NavbarComp.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
+import {Navbar, Nav, Container} from 'react-bootstrap'
 
 import {
     BrowserRouter as Router,
